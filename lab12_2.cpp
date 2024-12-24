203baf0b0e6864daa760ac8f27c5fe4511a90bd8
ErrkkSkyseer <lolplayer78876@gmail.com>
2024-12-24 15:15:18 +0700

    Lab12 Completed!!!!

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -1,9 +1,17 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int gcd(int, int);
+
+int main()
+{
     int x, y;
     cin >> x >> y;
-    cout << gcd(x,y);
-    return 0; 
+    cout << gcd(x, y);
+    return 0;
+}
+
+int gcd(int x, int y)
+{
+    return x % y == 0 ? y : gcd(y, x % y);
 }
